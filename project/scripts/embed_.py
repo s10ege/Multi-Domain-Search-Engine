ce6edc9620be83df640e8fc7f66eb8da58a0b2ea
s10ege <soneregeroglu18@hotmail.com>
2026-02-03 19:20:32 +0000

    Clean repo: remove all

--- project/scripts/embed_.py
+++
@@ -1,6 +0,0 @@
-from txtai import Embeddings
-from search_engine import load_resources, search
-
-resources = load_resources()
-results = search(resources, "machine learning" , top_k=10, domain="research")
-print(results)
